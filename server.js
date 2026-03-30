a3cf8c9556dff887b35e56f148aa7f3b7cc9988a
Hank <msamirajpurkar7@gmail.com>
2026-03-30 16:33:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,11 @@ const axios = require("axios");
 app.post("/send-otp", async (req, res) => {
 
   const { phone } = req.body;
+  const otp = Math.floor(100000 + Math.random() * 900000);
+
+  otpStore[phone] = otp;
+
+  console.log("OTP for", phone, "is", otp);
 
   try {
 

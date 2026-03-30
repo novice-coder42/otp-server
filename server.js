f03e7680417bbf18f2a1980f9fe3fdd5e463b445
Hank <msamirajpurkar7@gmail.com>
2026-03-30 15:32:31 +0530

    Second

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
-
 const app = express();
 
 app.use(cors());
@@ -9,7 +9,7 @@ app.use(express.json());
 let otpStore = {};
 
 // Send OTP
-app.post("/send-otp", (req, res) => {
+/*app.post("/send-otp", (req, res) => {
 
   const { phone } = req.body;
 
@@ -22,6 +22,38 @@ app.post("/send-otp", (req, res) => {
   // SMS API will be added later
 
   res.json({ success: true });
+
+});*/
+const axios = require("axios");
+
+app.post("/send-otp", async (req, res) => {
+
+  const { phone } = req.body;
+
+  const otp = Math.floor(100000 + Math.random() * 900000);
+
+  otpStore[phone] = otp;
+
+  console.log("OTP for", phone, "is", otp);
+
+  try {
+
+    await axios.post("https://api.msg91.com/api/v5/otp", {
+      mobile: "91" + phone,
+      otp: otp
+    }, {
+      headers: {
+        "Content-Type": "application/json",
+        "authkey": process.env.MSG91_API_KEY
+      }
+    });
+
+    res.json({ success: true });
+
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.json({ success: false });
+  }
 
 });
 
@@ -48,6 +80,7 @@ app.post("/verify-otp", (req, res) => {
 
 app.get("/", (req,res)=>{
   res.send("OTP Server Running");
+  console.log(process.env.MSG91_API_KEY);
 })
 
 app.listen(3000, () => {
